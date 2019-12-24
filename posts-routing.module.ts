42f743b4b78fd0ca79bb90aef893ec39d7ec9691
Emil Mammadov <emilmammadov303@gmail.com>
2019-12-25 02:35:24 +0300

    timeline, firestore map&reduce, addPosts, getPosts

--- posts-routing.module.ts
+++ posts-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'timeline',
+    pathMatch: 'full'
+  },
+  {
+    path: 'timeline',
+    loadChildren: './timeline/timeline.module#TimelinePageModule'
+  },
+  {
+    path: 'invitations',
+    loadChildren: './invitations/invitations.module#InvitationsPageModule'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- timeline/timeline.page.ts
+++ timeline/timeline.page.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirebaseService} from '../../services/firebaseService/firebase.service';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-timeline',
-  templateUrl: './timeline.page.html',
-  styleUrls: ['./timeline.page.scss'],
+    selector: 'app-timeline',
+    templateUrl: './timeline.page.html',
+    styleUrls: ['./timeline.page.scss'],
 })
 export class TimelinePage implements OnInit {
 
-  constructor() { }
+    uid: string;
+    posts: any[];
 
-  ngOnInit() {
-  }
+    constructor(private firebaseService: FirebaseService) {
+    }
+
+    ngOnInit() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.uid = user.uid;
+            this.getPosts(user.uid);
+
+        });
+    }
+
+    uploadPost() {
+        const post = {
+            body: 'denemedenemedenemedeneme',
+            city: {
+                name: 'AMASYA',
+                plate: 5
+            },
+            ownerID: this.uid,
+            ownerUsername: '@emilmammadov',
+            invitations: [],
+        };
+
+        this.firebaseService.uploadPost(this.uid, post);
+    }
+
+    getPosts(uid: string) {
+        this.firebaseService.getPosts(uid, posts => {
+            this.posts = posts;
+            console.log(posts);
+        });
+    }
 
 }
